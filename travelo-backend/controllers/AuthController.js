b478891e76a8a185e91a8db165c39f28770e02b6
sharif <sharifkamran8092@gmail.com>
2024-09-10 11:59:11 +0530

    backend transfer to my new reposoty with frontend

---
+++ travelo-backend/controllers/AuthController.js
@@ -0,0 +1,72 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const UserModel = require("../model/user.model");
+
+
+const signup = async (req, res) => {
+    try {
+        const { username, email, password , number } = req.body;
+        const user = await UserModel.findOne({ number });
+        if (user) {
+            return res.status(409)
+                .json({ message: 'User is already exist, you can login', success: false });
+        }
+        const userModel = new UserModel({ username, email, password , number });
+        userModel.password = await bcrypt.hash(password, 10);
+        await userModel.save();
+        res.status(201)
+            .json({
+                message: "Signup successfully",
+                success: true
+            })
+    } catch (err) {
+        res.status(500)
+            .json({
+                message: "Internal server errror",
+                success: false
+            })
+    }
+}
+
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        const errorMsg = 'Auth failed email or password is wrong';
+        if (!user) {
+            return res.status(403)
+                .json({ message: errorMsg, success: false });
+        }
+        const isPassEqual = await bcrypt.compare(password, user.password);
+        if (!isPassEqual) {
+            return res.status(403)
+                .json({ message: errorMsg, success: false });
+        }
+        const jwtToken = jwt.sign(
+            { email: user.email, _id: user._id },
+            process.env.PASSWORD_SECRET_KEY,
+            { expiresIn: '24h' }
+        )
+
+        res.status(200)
+            .json({
+                message: "Login Success",
+                success: true,
+                jwtToken,
+                email,
+                name: user.name
+            })
+    } catch (err) {
+        res.status(500)
+            .json({
+                message: "Internal server errror",
+                success: false
+            })
+    }
+}
+
+module.exports = {
+    signup,
+    login
+}
